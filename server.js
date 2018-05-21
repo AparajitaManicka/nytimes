cce82875e790762317ce93f7d3840118eea738b8
AparajitaManicka <appymonica88@gmail.com>
2018-05-21 12:05:21 -0400

    Change

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytimes";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
-var db = mongoose.connection;
+//var db = mongoose.connection;
 var newArticlesArray = [];
 
 // Routes
